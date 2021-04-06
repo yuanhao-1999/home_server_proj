5d8b59bf5153a36a066a159e1f852572e4cfbfc7
FishInWater-1999 <814484626@qq.com>
2021-04-06 20:46:28 +0800

    服务
    服务

--- home/src/main/java/com/horn/home/HomeApplication.java
+++ home/src/main/java/com/horn/home/HomeApplication.java
@@ -3,7 +3,7 @@ package com.horn.home;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages="controller")
+@SpringBootApplication
 public class HomeApplication {
 
 	public static void main(String[] args) {
